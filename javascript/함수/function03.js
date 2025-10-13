7743629c0541b77fae6b5fe0bd5365db3bd9829f
tndals11 <136726862+tndals11@users.noreply.github.com>
2025-10-13 14:32:27 +0900

    2025-10-13 JavaScript 함수 매개변수

---
+++ javascript/함수/function03.js
@@ -0,0 +1,69 @@
+
+
+
+
+// ! 2. JS에서의 매개변수, 인자 특징
+//  : 두 가지의 수가 반드시 일치할 필요 X
+
+function log(a) {
+  console.log(a);
+}
+
+log(); // 인자를 전달하지 않은 파라미터 값은 undefined : 변수를 선언하고 초기화 이전의 타입
+
+log('안녕'); // 안녕
+
+log('Hello', 'Hi'); // Hello
+// >> 여러 개의 파라미터, 인자값 나열 시 , 콤마로 구분
+//     지정된 수의 파라미터 이상의 인수는 읽히지 X (무시)
+
+function introduce(name, age) {
+  console.log(`${name} is ${age} years old`);
+}
+
+introduce('김승민', 50); 
+introduce(20, '김승민'); // >> 매개변수의 차례로 인자값이 전달 
+
+// ! 3. 반환값 (return, 리턴값)
+//  : 결과를 반환
+//  - 함수 내에서 return 키워드가 읽히면 함수의 실행을 즉시 중단, return 뒤의 값을 반환
+
+function subtract(a, b) {
+  let result = a - b;
+  return result;
+  // console.log('안녕하세요'); // Unreachable code detected. : 도달될 수 없는 코드 감지
+  // return과 같은 스코프에서 키워드 뒤의 코드는 읽히지 x 
+}
+
+let outcome = subtract(10, 7); // 3
+console.log(outcome);
+console.log(subtract(20, 1)); // 19
+
+// cf) return 키워드가 없는 함수는 실행 시 undefined를 반환
+function noReturn() {
+  console.log('Hello');
+}
+
+console.log(noReturn()); // undefined
+
+// ==  연습 문제 == //
+// - square 1, 2, 3 함수 작성 
+// - 파라미터를 x로 지정
+// - 반환 값으로 x의 제곱을 지정
+// - 함수 선언 방식 3가지 모두 사용 마지막 결과를 콘솔에 출력
+
+function square1(x) {
+  return x * x;
+}
+
+
+const square2 = function(x) {
+  return x * x; 
+}
+
+const square3 = x => x * x;
+
+
+console.log(square1(2));
+console.log(square2(4));
+console.log(square3(5));
