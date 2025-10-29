e87696d6da1400f788b65a7a238801bb70e00096
tndals11 <136726862+tndals11@users.noreply.github.com>
2025-10-29 12:03:52 +0900

    2025-10-29 React 컴포넌트 생성

---
+++ lean-react/src/basic/React.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// ! rfc: 리액트 함수형 컴포넌트 생성 
+// export default function React() {
+//   return (
+//     <div>React</div>
+//   )
+// }
+
+// ! rfce: 리액트 함수형 컴포넌트 생성
+import React from 'react'
+
+function React() {
+  return (
+    <div>React</div>
+  )
+}
+
+export default React
